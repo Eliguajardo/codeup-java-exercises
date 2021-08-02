48427955b31086e9c2577578280d683e1496e7f3
Elias Guajardo <eli.guajardo5@gmail.com>
2021-08-02 10:40:49 -0500

    ConsoleExercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.println("Pi is equal to " + pi);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter something: ");
+//        String userInput = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        System.out.println("Enter interger");
+
+        int userInput = scanner.nextInt();
+
+        System.out.println(userInput);
+
+
+    }
+}
---
+++ src/HelloPolaris.java
@@ -0,0 +1,28 @@
+
+
+//        System.out.println("Hello, World!");
+//        //1.
+//        int myFavoriteNumber = 32;
+//        System.out.println(myFavoriteNumber);
+//
+//        //2.
+//
+//       // String myString = "Hello World!";
+//       // System.out.println(myString);
+//        // variable myString is already defined in method main(java.lang.String[])
+//        //3.
+//        char myString;
+//        myString = 77;
+//        System.out.println(myString);
+//
+//        //4.
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+
+
+
+
