5cee8a78c75c9f3c31e57a840b9951b6289d65a0
Elias Guajardo <eli.guajardo5@gmail.com>
2021-08-02 10:56:31 -0500

    double pi

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
-//        double pi = 3.14159;
-//        System.out.println("Pi is equal to " + pi);
+        double pi = 3.14159;
+        System.out.println("Pi is equal to " + pi);
+        System.out.println("double : " + String.format("%.2f", pi));
 
-
-        Scanner scanner = new Scanner(System.in);
-
-//        System.out.print("Enter something: ");
-//        String userInput = scanner.next();
+//        Scanner scanner = new Scanner(System.in);// this allows scanner class to work with input
 //
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
-        System.out.println("Enter interger");
-
-        int userInput = scanner.nextInt();
-
-        System.out.println(userInput);
+////        System.out.print("Enter something: ");
+////        String userInput = scanner.next();
+////
+////        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.println("Enter interger");// request input
+//
+//        int userInput = scanner.nextInt();// stores input can bve float double ect
+//
+//        System.out.println(userInput); // output of what entered or calculate
 
 
     }
