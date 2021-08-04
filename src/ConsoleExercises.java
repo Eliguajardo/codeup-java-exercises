b849c810f011d53bc15b95508d5243efd8b506a1
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-08-03 21:58:02 -0500

    bob exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
-public class ConsoleExercises {
-    public static void main(String[] args) {
+//import java.util.Scanner;
+//public class ConsoleExercises {
+//    public static void main(String[] args) {
 //        double pi = 3.14159;
 //        System.out.println("Pi is equal to " + pi);
 //        System.out.println("double : " + String.format("%.2f", pi));
-
+//
 //        Scanner scanner = new Scanner(System.in);// this allows scanner class to work with input
 //
 ////        System.out.print("Enter something: ");
@@ -16,7 +16,7 @@ public class ConsoleExercises {
 //        int userInput = scanner.nextInt();// stores input can bve float double ect
 //
 //        System.out.println(userInput); // output of what entered or calculate
-
+//
 //        String Vatos = "Vatos";
 //        String Locos = "Locos";
 //        String forever = "Forever";
@@ -28,7 +28,7 @@ public class ConsoleExercises {
 //        forever = scanner.next();
 //        System.out.println("You entered: --> \"" + Vatos + Locos + forever + "\" <---");
 //
-
+//
 //        String Sentence;
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter a sentence: ");
@@ -51,35 +51,35 @@ public class ConsoleExercises {
 //        System.out.println("You entered: --> \"" + width + length + "\" <--");
 //        System.out.println("This is the area: --> \"" + area + "\" <-- ");
 //        System.out.println("This is the area: --> \"" + perimeter + "\" <-- ");
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print(" Enter the length  of the Codeup Classrom:");
-
-        String userInput = scanner.nextLine();
-        double UserNumbers = Integer.parseInt(userInput);
-        System.out.println("You entered Length: -->\" " +UserNumbers + "\" <--");
-
-
-        System.out.println("Enter the Width of the Classroom");
-        String UserInputWidth = scanner.nextLine();
-        double  UserWidth = Integer.parseInt(UserInputWidth);
-        System.out.println("You entered Width: -->\" " +UserInputWidth + "\" <--");
-
-
-        double area = UserNumbers * UserWidth;
-        double perimiter = area * 2;
-        System.out.printf("The area of the classroom is %s \n",area );
-        System.out.printf("The perimeter of the classroom is %s \n",perimiter);
-        //        System.out.println("You entered Width) \" " +area+ "\" \""+perimiter+"\"");
-
-
-       System.out.println("What is the height of the Codeup Classroom?:");
-        String userInputHeight = scanner.nextLine();
-        double UserHeight = Integer.parseInt(userInputHeight);
-        System.out.println("You entered Height: -->\" " +userInputHeight + "\" <--");
-        double volume = UserNumbers * UserWidth * UserHeight;
-        System.out.printf("The volume of the Codeup Classroom is: %s \n",volume);
-
-    }
-}
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print(" Enter the length  of the Codeup Classrom:");
+//
+//        String userInput = scanner.nextLine();
+//        double UserNumbers = Integer.parseInt(userInput);
+//        System.out.println("You entered Length: -->\" " +UserNumbers + "\" <--");
+//
+//
+//        System.out.println("Enter the Width of the Classroom");
+//        String UserInputWidth = scanner.nextLine();
+//        double  UserWidth = Integer.parseInt(UserInputWidth);
+//        System.out.println("You entered Width: -->\" " +UserInputWidth + "\" <--");
+//
+//
+//        double area = UserNumbers * UserWidth;
+//        double perimiter = area * 2;
+//        System.out.printf("The area of the classroom is %s \n",area );
+//        System.out.printf("The perimeter of the classroom is %s \n",perimiter);
+//        //        System.out.println("You entered Width) \" " +area+ "\" \""+perimiter+"\"");
+//
+//
+//       System.out.println("What is the height of the Codeup Classroom?:");
+//        String userInputHeight = scanner.nextLine();
+//        double UserHeight = Integer.parseInt(userInputHeight);
+//        System.out.println("You entered Height: -->\" " +userInputHeight + "\" <--");
+//        double volume = UserNumbers * UserWidth * UserHeight;
+//        System.out.printf("The volume of the Codeup Classroom is: %s \n",volume);
+//
+//    }
+//}
