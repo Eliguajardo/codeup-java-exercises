ad1ca0d28929d4858f0b67dd432885d3701fa59b
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-08-03 21:52:48 -0500

    completed

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String brickInWall = "We don't need no education \nWe don't need no thought control";
+        System.out.println(brickInWall);
+
+        String checkItOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkItOut);
+
+        String inWindows = "In windows, the main drive is usually C:\\";
+        System.out.println(inWindows);
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+
+    }
+}
