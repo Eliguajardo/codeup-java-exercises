53abc7c62080a92b8e2af2434e25599531d7fcf2
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-08-13 12:07:42 -0500

    changed some things in shapes

--- src/Shapes/Quadrilateral.java
+++ src/Shapes/Quadrilateral.java
@@ -1,35 +1,30 @@
-//package Shapes;
-//
-//import java.awt.*;
-//
-//public abstract class Quadrilateral extends Shape implements Measurable {
-//    protected int length;
-//    protected int width;
-//
-//    public  int getLength() {
-//        return length;
-//    }
-//
-//    public int getWidth() {
-//        return width;
-//    }
-//
-//    public Quadrilateral(int length, int width) {
-//        this.length = length;
-//        this.width = width;
-//    }
-//
-//    public Quadrilateral() {
-//    }
-//
-//    public  void setLength(int length){
-//        this.length = length;
-//    }
-//
-//
-//    public  void setWidth(int width){
-//        this.width = width;
-//
-//    }
-//
-//}
+package Shapes;
+
+import java.awt.*;
+
+public abstract class Quadrilateral implements Measurable, Shape {
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+}
